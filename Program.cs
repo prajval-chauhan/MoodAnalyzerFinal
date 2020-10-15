039905fd3f68ce511c18bdc778d9fc87a0930774
Prajval Chauhan <prajval.chauhan3@gmail.com>
2020-10-15 16:15:07 +0530

    Welcome Message Added

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodAnalyzerFinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Mood Analyzer Program\n");
+        }
+    }
+}

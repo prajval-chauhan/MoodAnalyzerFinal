fc4c4aa1646d69b06a1c107e0d2ebe3fcc5b9c06
Prajval Chauhan <prajval.chauhan3@gmail.com>
2020-10-15 17:00:06 +0530

    TC 1.1 and TC 1.2 created

---
+++ MoodAnalyse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyzerFinal
+{
+    public class MoodAnalyse
+    {
+        public string AnalyseMessage(string message)
+        {
+            string messageConverted = message.ToLower();
+            if (messageConverted.Contains("sad"))
+                return "SAD";
+            else
+                return "HAPPY";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,10 @@ namespace MoodAnalyzerFinal
     {
         static void Main(string[] args)
         {
+            MoodAnalyse call = new MoodAnalyse();
             Console.WriteLine("Welcome to the Mood Analyzer Program\n");
+            Console.WriteLine(call.AnalyseMessage("I am Happy"));
+            Console.WriteLine(call.AnalyseMessage("I am not feeling anything"));
         }
     }
 }
